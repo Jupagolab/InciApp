f07e53f884f985f6f5773166c14e4525109ce81e
Jupagolab <114892574+Jupagolab@users.noreply.github.com>
2024-11-27 16:56:32 -0400

    formulario

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Form from "./pages/Form";
 
 function App() {
 
   return (
-    <>
-  
-    </>
+    <div className="min-h-screen flex justify-center items-center mx-auto">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/formulario" element={<Form />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
--- frontend/src/pages/Form.jsx
+++ frontend/src/pages/Form.jsx
@@ -1,9 +1,57 @@
 const Form = () => {
+  const handleSubmit = () => {};
+
   return (
     <div>
-      
+      <form
+        onSubmit={handleSubmit}
+        className="grid grid-cols-1 gap-2 mx-auto p-4 max-w-80 bg-slate-200 rounded-md"
+      >
+        <h2 className="font-bold text-center mb-2">
+          Formulario de Incidencias
+        </h2>
+        <input
+          className="py-1 px-2"
+          type="text"
+          name="titulo"
+          id="titulo"
+          placeholder="Titulo de Incidencia"
+        />
+        <textarea
+          className="py-1 px-2"
+          name="descripcion"
+          id="descripcion"
+          cols="30"
+          rows="5"
+          placeholder="Descripción"
+        />
+        <select className="py-1 px-2" name="estado" id="estado">
+          <option value="Pendiente">Pendiente</option>
+          <option value="En Progreso">En Progreso</option>
+          <option value="Resuelto">Resuelto</option>
+        </select>
+        <div className="flex justify-start items-center gap-3">
+          <label className="mx-2 font-semibold" htmlFor="prioridad">
+            Prioridad
+          </label>
+          <select
+            className="py-1 px-2"
+            name="prioridad"
+            id="prioridad"
+            value={"Media"}
+          >
+            <option value="Baja">Baja</option>
+            <option value="Media">Media</option>
+            <option value="Alta">Alta</option>
+          </select>
+        </div>
+        <label className="font-semibold" htmlFor="fecha">
+          Fecha de Creación
+        </label>
+        <input type="date" name="fecha" id="fecha" className="py-1 px-2" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
